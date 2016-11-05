bb54a74fc6a9340029aa617818baaef282e3a446
bloodstalker <thabogre@gmail.com>
2016-11-05 04:02:56 +0330

    the misra-c check rules

---
+++ mutator-lvl0.cpp
@@ -0,0 +1,225 @@
+
+/*first line intentionally left blank.*/
+/**********************************************************************************************************************/
+/*included modules*/
+/*project headers*/
+#include "mutator_aux.h"
+/*standard headers*/
+#include <string>
+#include <iostream>
+#include <cassert>
+/*LLVM headers*/
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Lex/Lexer.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Function.h"
+/**********************************************************************************************************************/
+/*used namespaces*/
+using namespace llvm;
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::driver;
+using namespace clang::tooling;
+/**********************************************************************************************************************/
+/*global vars*/
+
+static llvm::cl::OptionCategory MatcherSampleCategory("Matcher Sample");
+/**********************************************************************************************************************/
+class MCForCmpless : public MatchFinder::MatchCallback {
+public:
+  MCForCmpless (Rewriter &Rewrite) : Rewrite (Rewrite) {}
+
+  virtual void run(const MatchFinder::MatchResult &MR)
+  {
+    if (MR.Nodes.getNodeAs<clang::ForStmt>("mcfor") != nullptr)
+    {
+      const ForStmt *FS = MR.Nodes.getNodeAs<clang::ForStmt>("mcfor");
+
+      SourceLocation SL = FS->getLocStart();
+      SL = Devi::SourceLocationHasMacro(SL, Rewrite, "start");
+
+      std::cout << "\"For\" statement has no braces {}:\n" << std::endl;
+      std::cout << SL.printToString(*MR.SourceManager) << "\n" << std::endl;
+    }
+    else
+    {
+      std::cout << "matcher -mcfor- returned nullptr." << std::endl;
+    }
+  }
+
+private:
+  Rewriter &Rewrite;
+};
+/**********************************************************************************************************************/
+class MCWhileCmpless : public MatchFinder::MatchCallback {
+public:
+  MCWhileCmpless (Rewriter &Rewrite) : Rewrite (Rewrite) {}
+
+  virtual void run(const MatchFinder::MatchResult &MR)
+  {
+    if (MR.Nodes.getNodeAs<clang::WhileStmt>("mcwhile") != nullptr)
+    {
+      const WhileStmt *WS = MR.Nodes.getNodeAs<clang::WhileStmt>("mcwhile");
+
+      SourceLocation SL = WS->getLocStart();
+      SL = Devi::SourceLocationHasMacro(SL, Rewrite, "start");
+
+      std::cout << "\"While\" statement has no braces {}:\n" << std::endl;
+      std::cout << SL.printToString(*MR.SourceManager) << "\n" << std::endl;
+    }
+    else
+    {
+      std::cout << "matcher -mcwhile- returned nullptr." << std::endl;
+    }
+  }
+
+private:
+  Rewriter &Rewrite;
+};
+/**********************************************************************************************************************/
+class MCElseCmpless : public MatchFinder::MatchCallback {
+public:
+  MCElseCmpless (Rewriter &Rewrite) : Rewrite (Rewrite) {}
+
+  virtual void run(const MatchFinder::MatchResult &MR)
+  {
+    if (MR.Nodes.getNodeAs<clang::IfStmt>("mcelse") != nullptr)
+    {
+      const IfStmt *IS = MR.Nodes.getNodeAs<clang::IfStmt>("mcelse");
+
+      SourceLocation SL = IS->getLocStart();
+      SL = Devi::SourceLocationHasMacro(SL, Rewrite, "start");
+
+      std::cout << "\"Else\" statement has no braces {}:\n" << std::endl;
+      std::cout << SL.printToString(*MR.SourceManager) << "\n" << std::endl;
+    }
+    else
+    {
+      std::cout << "matcher -mcelse- returned nullptr." << std::endl;
+    }
+  }
+
+private:
+  Rewriter &Rewrite;
+};
+/**********************************************************************************************************************/
+class MCIfCmpless : public MatchFinder::MatchCallback {
+public:
+  MCIfCmpless (Rewriter &Rewrite) : Rewrite (Rewrite) {}
+
+  virtual void run(const MatchFinder::MatchResult &MR)
+  {
+    if (MR.Nodes.getNodeAs<clang::IfStmt>("mcif") != nullptr)
+    {
+      const IfStmt *IS = MR.Nodes.getNodeAs<clang::IfStmt>("mcif");
+
+      SourceLocation SL = IS->getLocStart();
+      SL = Devi::SourceLocationHasMacro(SL, Rewrite, "start");
+
+      std::cout << "\"If\" statement has no braces {}:\n" << std::endl;
+      std::cout << SL.printToString(*MR.SourceManager) << "\n" << std::endl;
+    }
+    else
+    {
+      std::cout << "matcher -mcif- returned nullptr." << std::endl;
+    }
+  }
+
+private:
+  Rewriter &Rewrite;
+};
+/**********************************************************************************************************************/
+class IfElseMissingFixer : public MatchFinder::MatchCallback
+{
+public:
+  IfElseMissingFixer (Rewriter &Rewrite) : Rewrite (Rewrite) {}
+
+  virtual void run(const MatchFinder::MatchResult &MR)
+  {
+    if (MR.Nodes.getNodeAs<clang::IfStmt>("mcifelse") != nullptr)
+    {
+      const IfStmt *ElseIf = MR.Nodes.getNodeAs<clang::IfStmt>("mcifelse");
+
+      SourceLocation IFESL = ElseIf->getLocStart();
+      IFESL = Devi::SourceLocationHasMacro(IFESL, Rewrite, "start");
+
+      std::cout << "\"If-Else If\" statement has no ending Else:\n" << std::endl;
+      std::cout << IFESL.printToString(*MR.SourceManager) << "\n" << std::endl;
+    }
+    else
+    {
+      std::cout << "matcher -mcifelse- returned nullptr." << std::endl;
+    }
+  }
+
+
+private:
+  Rewriter &Rewrite;
+};
+/**********************************************************************************************************************/
+/**********************************************************************************************************************/
+class MyASTConsumer : public ASTConsumer {
+
+public:
+  MyASTConsumer(Rewriter &R) : HandlerForCmpless(R), HandlerWhileCmpless(R), HandlerElseCmpless(R), HandlerIfCmpless(R), HandlerForIfElse(R) {
+    /*forstmts whithout a compound statement.*/
+    Matcher.addMatcher(forStmt(unless(hasDescendant(compoundStmt()))).bind("mcfor"), &HandlerForCmpless);
+
+    /*whilestmts without a compound statement.*/
+    Matcher.addMatcher(whileStmt(unless(hasDescendant(compoundStmt()))).bind("mcwhile"), &HandlerWhileCmpless);
+
+    /*else blocks that dont have a compound statemnt.*/
+    Matcher.addMatcher(ifStmt(allOf(hasElse(unless(ifStmt())), hasElse(unless(compoundStmt())))).bind("mcelse"), &HandlerElseCmpless);
+
+    /*if blocks that dont have a compound statement.*/
+    Matcher.addMatcher(ifStmt(unless(hasDescendant(compoundStmt()))).bind("mcif"), &HandlerIfCmpless);
+
+    /*if-elseif statements that are missing the else block.*/
+    Matcher.addMatcher(ifStmt(allOf(hasElse(ifStmt()), unless(hasAncestor(ifStmt())), unless(hasDescendant(ifStmt(hasElse(unless(ifStmt()))))))).bind("mcifelse"), &HandlerForIfElse);
+  }
+
+  void HandleTranslationUnit(ASTContext &Context) override {
+    Matcher.matchAST(Context);
+  }
+
+private:
+  MCForCmpless HandlerForCmpless;
+  MCWhileCmpless HandlerWhileCmpless;
+  MCElseCmpless HandlerElseCmpless;
+  MCIfCmpless HandlerIfCmpless;
+  IfElseMissingFixer HandlerForIfElse;
+  MatchFinder Matcher;
+};
+/**********************************************************************************************************************/
+class MyFrontendAction : public ASTFrontendAction {
+public:
+  MyFrontendAction() {}
+  void EndSourceFileAction() override {
+    TheRewriter.getEditBuffer(TheRewriter.getSourceMgr().getMainFileID()).write(llvm::outs());
+  }
+
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI, StringRef file) override {
+    TheRewriter.setSourceMgr(CI.getSourceManager(), CI.getLangOpts());
+    return llvm::make_unique<MyASTConsumer>(TheRewriter);
+  }
+
+private:
+  Rewriter TheRewriter;
+};
+/**********************************************************************************************************************/
+/*Main*/
+int main(int argc, const char **argv) {
+  CommonOptionsParser op(argc, argv, MatcherSampleCategory);
+  ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+  return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+}
+/*last line intentionally left blank.*/
