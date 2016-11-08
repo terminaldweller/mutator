bc8a21c92c42ca1219efaba49f30fa0062be0555
bloodstalker <thabogre@gmail.com>
2016-11-08 20:18:34 +0330

    the folder thats gonna contain some tools to ease the dev process. dumper dumps the AST.

---
+++ extra-tools/dumper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+INPUT="./testFuncs1.c"
+OUTPUT="./dump"
+OPTIONS="--"
+RUN="1"
+
+while [[ $# -gt 0 ]]
+do
+	passarg="$1"
+
+	case $passarg in
+		-i|--input)
+		INPUT="$2"
+		shift
+		;;
+		-o|--output)
+		OUTPUT="$2"
+		shift
+		;;
+		-op|--options)
+		OPTIONS="$2"
+		shift
+		;;
+		-h|--help)
+		echo "the script simply dumps the AST of the input."
+		echo "-i|--input 	the input file full path."
+		echo "-o|--output 	the output fule full path."
+		echo "-op|--options 	extra options to pass to clang"
+		echo "-h|--help 	prints out the help."
+		RUN="0"
+		;;
+		*)
+		echo $1 is not a valid argument...
+		RUN="0"
+		shift
+	esac
+	shift
+done
+
+if [[ "$RUN" == "1" ]]; then
+	"/home/bloodstalker/llvm/llvm/build/bin/clang" -Xclang -ast-dump -fsyntax-only "$INPUT" "$OPTIONS" > "$OUTPUT"
+fi
