5ca610289d8d7a7cbf0af2ad7ba305a5a4245d9b
bloodstalker <thabogre@gmail.com>
2016-11-30 12:51:29 +0330

    added xml output support for the misra report

--- mutator_aux.h
+++ mutator_aux.h
@@ -5,11 +5,28 @@
 #include <string>
 #include "clang/AST/AST.h"
 #include "clang/Rewrite/Core/Rewriter.h"
+#include "tinyxml2/tinyxml2.h"
 
 using namespace clang;
+using namespace tinyxml2;
 
 namespace Devi {
 SourceLocation SourceLocationHasMacro (SourceLocation SL, Rewriter &Rewrite, std::string Kind);
+
+class XMLReport
+{
+public:
+  XMLReport();
+
+  void XMLCreateReport(void);
+  void XMLAddNode(ASTContext* ASTC, SourceLocation SL, std::string MisraRule, std::string Description);
+  void SaveReport(void);
+
+private:
+  XMLDocument XMLReportDoc;
+  XMLNode* RootPointer;
+};
+
 }
 
 #endif
