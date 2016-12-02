0097ae539f05eb9876203c5d5188c57284e47e5c
bloodstalker <thabogre@gmail.com>
2016-12-02 08:27:30 +0330

    added a disclaimer

--- mutator.cpp
+++ mutator.cpp
@@ -1,5 +1,6 @@
 
 /*first line intentionally left blank.*/
+/*code structure inspired by Eli Bendersky's tutorial on Rewriters.*/
 /**********************************************************************************************************************/
 /*FIXME-all classes should use replacements.*/
 /**********************************************************************************************************************/
