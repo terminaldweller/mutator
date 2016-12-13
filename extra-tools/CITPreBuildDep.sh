19d15514d72f06c6c9f3f3bfc1b716c3b13f500b
bloodstalker <thabogre@gmail.com>
2016-12-13 23:07:07 +0330

    trying to add ci travis support

--- extra-tools/CITPreBuildDep.sh
+++ extra-tools/CITPreBuildDep.sh
@@ -29,14 +29,14 @@
 "sudo" update-alternatives --install /usr/bin/cmake cmake /usr/local/bin/cmake 1 --force
 
 #building llvm and clang 4.0.0
-"cd" ~
-"mkdir" llvm-clang4
-"cd" llvm-clang4
-"svn" co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-"mkdir" build
-"cd" ./llvm/tools
-"svn" co http://llvm.org/svn/llvm-project/cfe/trunk clang
-"cd" ../..
-"cd" build
-"cmake" -G "Unix Makefiles" ../llvm -DC_BUILD_TYPE=Release
-"make"
+#"cd" ~
+#"mkdir" llvm-clang4
+#"cd" llvm-clang4
+#"svn" co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+#"mkdir" build
+#"cd" ./llvm/tools
+#"svn" co http://llvm.org/svn/llvm-project/cfe/trunk clang
+#"cd" ../..
+#"cd" build
+#"cmake" -G "Unix Makefiles" ../llvm -DC_BUILD_TYPE=Release
+#"make"
