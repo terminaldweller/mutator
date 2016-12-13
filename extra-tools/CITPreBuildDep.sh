1ecf1cb4150cefbb2d283b6a2d9105a5dc6c939d
bloodstalker <thabogre@gmail.com>
2016-12-13 16:43:25 +0330

    added a pre-build script for CI Travis to run

---
+++ extra-tools/CITPreBuildDep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+"cd" ~
+"mkdir" llvm-clang4
+"cd" llvm-clang4
+"svn" co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+"mkdir" build
+"cd" ./llvm/tools
+"svn" co http://llvm.org/svn/llvm-project/cfe/trunk clang
+"cd" ../..
+"cd" build
+"cmake" -G "Unix Makefiles" ../llvm -DC_MAKE_TYPE=Release
+"make"
