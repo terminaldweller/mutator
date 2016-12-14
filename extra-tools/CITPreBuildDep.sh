1d0e77aa3e4117bc89d2aa7b8911693fbc27937e
bloodstalker <thabogre@gmail.com>
2016-12-15 01:55:26 +0330

    update

--- extra-tools/CITPreBuildDep.sh
+++ extra-tools/CITPreBuildDep.sh
@@ -3,21 +3,21 @@
 "sudo" apt-get -qq update
 
 #installing libstdc++ 4.8
-"gcc" -v
-"sudo" add-apt-repository ppa:ubuntu-toolchain-r/test
-"sudo" apt-get update
-"sudo" update-alternatives --remove-all gcc
-"sudo" update-alternatives --remove-all g++
-"sudo" apt-get install gcc-4.8
-"sudo" apt-get install g++-4.8
-"sudo" update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-"sudo" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
-"sudo" update-alternatives --config gcc
-"sudo" update-alternatives --config g++
-"sudo" apt-get update
-"sudo" apt-get upgrade -y
-"sudo" apt-get dist-upgrade
-"gcc" -v
+#"gcc" -v
+#"sudo" add-apt-repository ppa:ubuntu-toolchain-r/test
+#"sudo" apt-get update
+#"sudo" update-alternatives --remove-all gcc
+#"sudo" update-alternatives --remove-all g++
+#"sudo" apt-get install gcc-4.8
+#"sudo" apt-get install g++-4.8
+#"sudo" update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+#"sudo" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+#"sudo" update-alternatives --config gcc
+#"sudo" update-alternatives --config g++
+#"sudo" apt-get update
+#"sudo" apt-get upgrade -y
+#"sudo" apt-get dist-upgrade
+#"gcc" -v
 
 #building cmkae 3.4.3 since the CI Travis virtual host is Ubuntu(???)
 "wget" http://www.cmake.org/files/v3.4/cmake-3.4.3.tar.gz
