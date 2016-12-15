6b024a5a2a9dbfba4a824477816dc4bc7499b9db
bloodstalker <thabogre@gmail.com>
2016-12-15 23:28:57 +0330

    not removing the script. it might come in handy later or users might need it to build llvm-clang.

--- extra-tools/CITPreBuildDep.sh
+++ extra-tools/CITPreBuildDep.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-#"sudo" apt-get -qq update
+"sudo" apt-get -qq update
 
 #installing libstdc++ 4.8
-#"gcc" -v
-#"sudo" add-apt-repository ppa:ubuntu-toolchain-r/test
-#"sudo" apt-get update
-#"sudo" update-alternatives --remove-all gcc
-#"sudo" update-alternatives --remove-all g++
-#"sudo" apt-get install gcc-4.8
-#"sudo" apt-get install g++-4.8
-#"sudo" update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-#"sudo" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
-#"sudo" update-alternatives --config gcc
-#"sudo" update-alternatives --config g++
-#"sudo" apt-get update
-#"sudo" apt-get upgrade -y
-#"sudo" apt-get dist-upgrade
-#"gcc" -v
+"gcc" -v
+"sudo" add-apt-repository ppa:ubuntu-toolchain-r/test
+"sudo" apt-get update
+"sudo" update-alternatives --remove-all gcc
+"sudo" update-alternatives --remove-all g++
+"sudo" apt-get install gcc-4.8
+"sudo" apt-get install g++-4.8
+"sudo" update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+"sudo" update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+"sudo" update-alternatives --config gcc
+"sudo" update-alternatives --config g++
+"sudo" apt-get update
+"sudo" apt-get upgrade -y
+"sudo" apt-get dist-upgrade
+"gcc" -v
 
 #building cmkae 3.4.3 since the CI Travis virtual host is Ubuntu(???)
 "wget" http://www.cmake.org/files/v3.4/cmake-3.4.3.tar.gz
@@ -30,15 +30,15 @@
 "cd" ..
 
 #building llvm and clang 4.0.0
-#"mkdir" llvm-clang4
-#"cd" llvm-clang4
-#"svn" co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-#"mkdir" build
-#"cd" ./llvm/tools
-#"svn" co http://llvm.org/svn/llvm-project/cfe/trunk clang
-#"cd" ../..
-#"cd" build
-#"cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../llvm
-#"make" -j8
-#"sudo" make install
-#"cd" ../..
+"mkdir" llvm-clang4
+"cd" llvm-clang4
+"svn" co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+"mkdir" build
+"cd" ./llvm/tools
+"svn" co http://llvm.org/svn/llvm-project/cfe/trunk clang
+"cd" ../..
+"cd" build
+"cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ../llvm
+"make" -j8
+"sudo" make install
+"cd" ../..
