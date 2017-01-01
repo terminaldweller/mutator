b7cdec5f7c8eaa19bf024d5acfc524c1e9e59ced
bloodstalker <thabogre@gmail.com>
2017-01-01 04:16:18 +0330

    prettier prints

--- extra-tools/ReportPrintPretty.sh
+++ extra-tools/ReportPrintPretty.sh
@@ -5,7 +5,13 @@
 BEGIN{RS = "\n";FS = ":"}
 function red(s) {printf "\033[1;31m" s "\033[0m "}
 function green(s) {printf "\033[1;32m" s "\033[0m "}
+function yellow(s) {printf "\033[1;33m" s "\033[0m "}
 function blue(s) {printf "\033[1;34m" s "\033[0m "}
-	{print red($1)green($2)green($3)blue($4)blue($5)}
+function purple(s) {printf "\033[1;35m" s "\033[0m "}
+function cyan(s) {printf "\033[1;36m" s "\033[0m "}
+function unred(s) {printf "\033[1;41m" s "\033[0m "}
+function ungreen(s) {printf "\033[1;42m" s "\033[0m "}
+function unyellow(s) {printf "\033[1;43m" s "\033[0m "}
+	{print red($1)green($2)purple($3)blue($4)blue($5)}
 END{}
 ' > "$2"
