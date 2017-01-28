ff28331ee29ed5277203b277984e6598da7f48be
bloodstalker <thabogre@gmail.com>
2017-01-29 00:15:44 +0330

    added more tdd tests for automatic refactoring facilities

--- test/auto-refac-tdd/auto-refac-tdd01.c
+++ test/auto-refac-tdd/auto-refac-tdd01.c
@@ -23,6 +23,106 @@ void tdd_forfixer(void)
 
   for (loopcounter = 0; loopcounter < 10; ++loopcounter)
     counter--       ;
+
+  for (loopcounter = 0; loopcounter < 10; ++loopcounter)
+  {
+    counter++;
+  }
+}
+
+void tdd_whilefixer(void)
+{
+  int whilecounter;
+
+  while (true)
+    whilecounter;
+
+  while (true)
+    whilecounter++     ;
+
+  while (true)
+  {
+    counter++;
+  }
+}
+
+void tdd_ifelsefixer(void)
+{
+  int a0;
+  int b0;
+
+  if (0 == a0)
+  {
+    /*something*/
+  }
+  else
+    a0++;
+
+  if (0 == a0)
+  {
+    /*something*/
+  }
+  else
+    a0++     ;
+
+  if (0 == a0)
+  {
+    /*something*/
+  }
+  else
+  {
+    a0++;
+  }
+
+  if (0 == a0)
+  {
+    if (0 == b0)
+    {
+
+    }
+    else
+      b0++;
+  }
+  else
+    a0++;
+
+}
+
+void tdd_iffixer(void)
+{
+  int a1;
+  int b1;
+
+  if (a1 > 10)
+    a1++;
+
+  if (b1 < 10)
+    a1++  ;
+  else if (a1 == 0)
+    b1--      ;
+  else
+  {
+
+  }
+
+  if (a1 > 10)
+    a1++;
+
+  if (b1 < 10)
+    a1++  ;
+  else if (a1 == 0)
+  {
+    b1--      ;
+    if (a1 < 10)
+      a1--;
+    else if (b1 > 10)
+      b1++;
+  }
+  else
+  {
+
+  }
+
 }
 
 void tdd_ifconstswapper(void)
