2c2d964d9cb0aaf6ca82ef9776ce636a776ef3ef
bloodstalker <thabogre@gmail.com>
2017-02-28 18:26:41 +0330

    daemon header

---
+++ daemon/mutatord.h
@@ -0,0 +1,29 @@
+
+/***************************************************Project Mutator****************************************************/
+/*first line intentionally left blank.*/
+/*the source code for the static checks(Misra-C,...)*/
+/*Copyright (C) 2017 Farzad Sadeghi
+This header for the daemon.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
+/**********************************************************************************************************************/
+#ifndef __MUTATOR_D_H
+#define __MUTATOR_D_H
+
+
+
+#endif
+/*last line intentionally left blank*/
+
