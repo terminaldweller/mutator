1ee95a9c937c56d3d7a56aaf6aae8ab9ccaf9190
bloodstalker <thabogre@gmail.com>
2017-03-06 13:17:46 +0330

    the header for mutator_aux.h

---
+++ daemon/daemon_aux.h
@@ -0,0 +1,31 @@
+
+/***************************************************Project Mutator****************************************************/
+/*first line intentionally left blank.*/
+/*Copyright (C) 2017 Farzad Sadeghi
+This header is for the server called by the daemon.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
+/**********************************************************************************************************************/
+#ifndef __MUTATOR_AUX_SERVER_H
+#define __MUTATOR_AUX_SERVER_H
+/**********************************************************************************************************************/
+/*headers*/
+/**********************************************************************************************************************/
+/*function prototypes*/
+int mutator_server(void);
+
+#endif
+/*last line intentionally left blank*/
+
