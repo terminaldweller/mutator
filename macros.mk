15d9d8b347596a60bf87941b9a3d0bf58d0fc9bc
bloodstalker <thabogre@gmail.com>
2017-03-12 17:38:37 +0330

    added some macros for the ctags target

--- macros.mk
+++ macros.mk
@@ -1,3 +1,7 @@
 #MACRO DEFINITIONS FOR MUTATOR BUILD
 
 CXX=clang++
+
+SRCS=./mutator_aux.cpp ./mutator-lvl1.cpp ./mutator-lvl0.cpp ./mutator-lvl2.cpp ./mutator-lvl0.h ./mutator_aux.h ./daemon/mutatord.h ./daemon/mutatorclient.c ./daemon/mutatorclient.h ./daemon/daemon_aux.h ./daemon/daemon_aux.c ./daemon/mutatord.c ./daemon/mutatorserver.c ./daemon/mutatorserver.h
+
+CTAGS=ctags --c++-kinds=+p --fields=+iaS --extra=+q 
