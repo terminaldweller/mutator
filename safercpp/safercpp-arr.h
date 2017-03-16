ee786e5daad2097ec0bc0a0e36f03e5b622cae5b
bloodstalker <thabogre@gmail.com>
2017-03-17 01:15:17 +0330

    the header

---
+++ safercpp/safercpp-arr.h
@@ -0,0 +1,33 @@
+
+/***************************************************Project Mutator****************************************************/
+//-*-c++-*-
+/*first line intentionally left blank.*/
+/*the header for SaferCPP's automatic refactoring of C/C++ arrays.*/
+/*Copyright (C) 2017 Noah L.,Farzad Sadeghi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
+/*code structure inspired by Eli Bendersky's tutorial on Rewriters.*/
+/**********************************************************************************************************************/
+/*inclusion gaurd*/
+#ifndef __SAFERCPP_ARR_H
+#define __SAFERCPP_ARR_H
+/**********************************************************************************************************************/
+#define __NO_XML
+#define __NO_JSON
+/**********************************************************************************************************************/
+#endif
+/**********************************************************************************************************************/
+/*last line intentionally left blank*/
+
