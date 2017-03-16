a6d0966126ae0376a0209bb94cba589f9c10c5c4
bloodstalker <thabogre@gmail.com>
2017-03-17 01:13:31 +0330

    added a new inclusion directive for the new report files

--- mutator-lvl0.cpp
+++ mutator-lvl0.cpp
@@ -24,6 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*
 /*project headers*/
 #include "mutator-lvl0.h"
 #include "mutator_aux.h"
+#include "mutator_report.h"
 /*standard headers*/
 #include <cassert>
 #include <exception>
