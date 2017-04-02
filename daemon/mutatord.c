dae64986f26d1e49e65b1657ef74f4f791ef852d
bloodstalker <thabogre@gmail.com>
2017-04-02 16:36:49 +0430

    the daemon returns the server exit code

--- daemon/mutatord.c
+++ daemon/mutatord.c
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*
 /**********************************************************************************************************************/
 /*macros*/
 #define __DBG
-#if 0
+#if 1
 #undef __DBG
 #endif
 /**********************************************************************************************************************/
@@ -140,7 +140,7 @@ int main(void)
     fclose(mut_log);
   }while(0);
 
-  //return server_exit_code;
+  return server_exit_code;
 
   /*@DEVI-these obviously will never run. theyre just a reminder that i need to handle the gracefull shutdown*/
 #if 0
