7ce41b0c65b98f76c7a7da27dc16917f0c80c0c6
bloodstalker <thabogre@gmail.com>
2017-05-15 15:02:38 +0430

    removed a meaningless comment

--- bruiser/bruiser-extra.h
+++ bruiser/bruiser-extra.h
@@ -18,7 +18,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*/
-/*code structure inspired by Eli Bendersky's tutorial on Rewriters.*/
 /**********************************************************************************************************************/
 /*inclusion guard*/
 #ifndef BRUISER_EXTRA_H
