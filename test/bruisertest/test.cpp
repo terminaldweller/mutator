d227ccd6146730694779ea43ebf5834c18f3da5b
bloodstalker <thabogre@gmail.com>
2017-06-10 20:38:54 +0430

    tests for bruiser

---
+++ test/bruisertest/test.cpp
@@ -0,0 +1,21 @@
+
+namespace devi
+{
+  class LOCO
+  {
+    public:
+      LOCO () {}
+
+      void lupo (void)
+      {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+      }
+  };
+}
+
+int main(int argc, const char **argv)
+{
+
+}
