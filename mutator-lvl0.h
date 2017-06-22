08be1c18119c12e7c5cfac3376e3228a9ee4de22
bloodstalker <thabogre@gmail.com>
2017-06-22 21:25:29 +0430

    included cassert

--- mutator-lvl0.h
+++ mutator-lvl0.h
@@ -30,6 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*
 #include <iostream>
 #include <map>
 #include <string>
+#include <cassert>
 #include <vector>
 #include <unordered_map>
 /*clang headers*/
