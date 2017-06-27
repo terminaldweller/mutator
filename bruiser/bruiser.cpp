2d67bf1770010e542528b7db95726631b8bc4e9c
bloodstalker <thabogre@gmail.com>
2017-06-27 23:46:50 +0430

    updated

--- bruiser/bruiser.cpp
+++ bruiser/bruiser.cpp
@@ -25,6 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*
 #include "CompletionHints.h"
 #include "../mutator_aux.h"
 #include "mutagen.h"
+#include "ORCmutation.h"
 /*standard headers*/
 #include <fstream>
 #include <string>
