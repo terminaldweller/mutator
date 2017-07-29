3fdc87fcd86803603ff8cf58e23dfdeafcffec82
bloodstalker <thabogre@gmail.com>
2017-07-29 19:09:53 +0430

    smoke test for m0 cpp

---
+++ test/m0-cpp-smoke/m0-cpp-smoke.cpp
@@ -0,0 +1,96 @@
+#include <cstdlib>
+#include <csignal>
+#include <csetjmp>
+#include <cstdarg>
+#include <typeinfo>
+#include <typeindex>
+#include <type_traits>
+#include <bitset>
+#include <functional>
+#include <utility>
+#include <ctime>
+#include <chrono>
+#include <cstddef>
+#include <initializer_list>
+#include <tuple>
+//c++17
+#if 0
+#include <any>
+#include <optional>
+#include <variant>
+#endif 
+#include <new>
+#include <memory>
+#include <scoped_allocator>
+#if 0
+#include <memory_resource>
+#endif
+#include <climits>
+#include <cfloat>
+#include <cstdint>
+#include <cinttypes>
+#include <limits>
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+#include <system_error>
+#include <cerrno>
+#include <cctype>
+#include <cwctype>
+#include <cstring>
+#include <cwchar>
+#include <cuchar>
+#include <string>
+#if 0
+#include <string_view>
+#endif
+#include <array>
+#include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#if 0
+#include <execution>
+#endif
+#include <iterator>
+#include <cmath>
+#include <complex>
+#include <valarray>
+#include <random>
+#include <numeric>
+#include <ratio>
+#include <cfenv>
+#include <iosfwd>
+#include <ios>
+#include <istream>
+#include <ostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <strstream>
+#include <iomanip>
+#include <streambuf>
+#include <cstdio>
+#include <locale>
+#include <clocale>
+#include <codecvt>
+#include <regex>
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
+#include <future>
+#include <condition_variable>
+#if 0
+#include <filesystem>
+#endif
+
+int main(int argc, char** argv)
+{}
