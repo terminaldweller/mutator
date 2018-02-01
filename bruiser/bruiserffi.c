edd1e1e47dd542ea9a4e04e7abadcfc44e3f1488
bloodstalker <thabogre@gmail.com>
2018-02-01 14:43:02 +0330

    removed capstone for the time being

--- bruiser/bruiserffi.c
+++ bruiser/bruiserffi.c
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.*
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-#include <capstone/capstone.h>
+//#include <capstone/capstone.h>
 #include "bruiserffi.h"
 /**********************************************************************************************************************/
 #define VOIDIFY(X) (void*)X
