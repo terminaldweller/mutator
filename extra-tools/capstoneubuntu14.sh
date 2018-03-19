65673508b45fea05f84039edc7751cefcdf785fc
bloodstalker <thabogre@gmail.com>
2018-03-19 18:24:48 +0330

    fix for travis

--- extra-tools/capstoneubuntu14.sh
+++ extra-tools/capstoneubuntu14.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 
 "wget" https://github.com/aquynh/capstone/archive/3.0.5-rc2.tar.gz -o capstone.tar.gz
-"tar" -xvzf capstone.tar.gz
+"tar" -xvzf 3.0.5-rc2.tar.gz
 "cd" capstone*
 "make"
 sudo make install
--- extra-tools/keystoneubuntu.sh
+++ extra-tools/keystoneubuntu.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 
 "wget" https://github.com/keystone-engine/keystone/archive/0.9.1.tar.gz -o keystone.tar.gz
-"tar" -xvzf keystone.tar.gz
+"tar" -xvzf 0.9.1.tar.gz
 "cd" keystone*
 "mkdir" build
 "cd" build
