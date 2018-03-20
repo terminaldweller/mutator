8520e0e93e47e6526b39545ed0bf9fca52354f6b
bloodstalker <thabogre@gmail.com>
2018-03-20 14:41:56 +0330

    run a deepclean on bruiser's makefile to rebuild the lua makefile

--- bruiser/asm.sh
+++ bruiser/asm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $0)
-clang ./asmrewriter.c -llua -o asmrewriter
+clang ./asmrewriter.c -L./lua-5.3.4/src -llua -lm -ldl -lreadline -o asmrewriter 
 ./asmrewriter ./lua-scripts/asmtest.lua || exit 1
 rm ./asmrewriter
