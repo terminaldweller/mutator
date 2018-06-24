dccbcf0295105f2ec1631443bb01a6d31d2b0dfb
bloodstalker <thabogre@gmail.com>
2018-06-24 11:28:10 +0430

    fixing travis

--- bruiser/tablegen.sh
+++ bruiser/tablegen.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 cd $(dirname $0)
 ../extra-tools/luatablegen.py --tbg ./wasmtablegen.json --out ./luatablegen --luaheader ../lua-5.3.4/src --pre ./luatablegen/wasmheader.txt --headeraggr ./luatablegen/wasm_tables.h --lualibpath ./lua-scripts/wasm.lua --docpath /home/bloodstalker/extra/mutator.wiki/wasm.md
 if [[ $1 == test ]]; then
