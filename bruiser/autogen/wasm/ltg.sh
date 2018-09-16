b0ce887ac8ee6f8d88f53a3ed45681beae151840
bloodstalker <thabogre@gmail.com>
2018-09-16 23:51:12 +0430

    a quick fix

--- bruiser/autogen/wasm/ltg.sh
+++ bruiser/autogen/wasm/ltg.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/sh
 cd $(dirname $0)
 "../../tablegen/luatablegen.py" --out ./ltg/ --luaheader ../../../lua-5.3.4/src --headeraggr ./ltg/wasm_tables.h --lualibpath ./ltg/wasm.lua --docpath ./ltg/wasm.md --xml ./ltg/wasm.xml --tbldefs ./ltg/ --name wasm
 clang-format ./ltg/*.c ./ltg/*.h -i
