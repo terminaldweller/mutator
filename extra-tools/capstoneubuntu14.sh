c2826edb6de9f17936d87cd356c3e1b105384508
bloodstalker <thabogre@gmail.com>
2019-12-06 13:07:36 +0330

    travis fix [appveyor skip]

--- extra-tools/capstoneubuntu14.sh
+++ extra-tools/capstoneubuntu14.sh
@@ -4,8 +4,7 @@ cd $(dirname $0)
 
 "wget" https://github.com/aquynh/capstone/archive/3.0.5-rc2.tar.gz -o capstone.tar.gz
 "tar" -xvzf 3.0.5-rc2.tar.gz
-"ls" capstone*
-"cd" capstone*
+"cd" capstone-3.0.5-rc2
 "make"
 sudo make install
 "cd" ..
