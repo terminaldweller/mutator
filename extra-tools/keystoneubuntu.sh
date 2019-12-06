247efb34bb2b88488322aba59228aea1d86b3fff
bloodstalker <thabogre@gmail.com>
2019-12-06 13:13:48 +0330

    travis fix [appveyor skip]

--- extra-tools/keystoneubuntu.sh
+++ extra-tools/keystoneubuntu.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 "wget" https://github.com/keystone-engine/keystone/archive/0.9.1.tar.gz -o keystone.tar.gz
 "tar" -xvzf 0.9.1.tar.gz
-"cd" keystone*
+"cd" keystone-0.9.1
 "mkdir" build
 "cd" build
 ../make-share.sh
